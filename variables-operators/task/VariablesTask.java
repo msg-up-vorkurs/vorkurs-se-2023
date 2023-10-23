fd54e5f18bb563952e57d8a89c763fb6d9a37423
danielschaubschlaeger <daniel.schaubschlaeger@msg.group>
2023-10-23 08:59:38 +0200

    Aufgaben zu Variablen und Operatoren

---
+++ variables-operators/task/VariablesTask.java
@@ -0,0 +1,20 @@
+package task;
+
+public class VariablesTask {
+
+    public static void main(String[] args) {
+
+        /*
+         * TODO Aufgabe 1: Deklarieren und Initialisieren Sie Variablen für die Beschreibung eines Computers
+         *  (Hersteller, Preis, Farbe, Lüfter, Arbeitsspeicher, etc.).
+         */
+
+
+        /*
+         * TODO Aufgabe 1: Ausgabe der Variablen
+         *  z.B. System.out.println("Hersteller: " + manufacturer);
+         */
+
+
+    }
+}
